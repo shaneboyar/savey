1d15d843c9680278f8fe017f6bf7dfc6128f1371
Shane <shane.boyar@gmail.com>
2019-01-04 16:18:08 -0500

    annotate

--- app/models/impulse.rb
+++ app/models/impulse.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: impulses
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  price      :decimal(, )
+#  remind_at  :datetime
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Impulse < ApplicationRecord
   belongs_to :user
 end
