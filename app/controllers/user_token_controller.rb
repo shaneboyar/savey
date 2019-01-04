f168a74539ecc2244e97aa6398de47b6f367cbf4
Shane <shane.boyar@gmail.com>
2019-01-04 18:12:29 -0500

    tokens and cookies but not great

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,8 +1,14 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
   before_action :require_confirmation, only: [:create]
+  after_action :store_jwt_in_cookie, only: [:create]
 
   private
+
+  def store_jwt_in_cookie
+    jwt = JSON.parse(response.body)["jwt"]
+    cookies[:jwt] = {value:  jwt, expires: 1.day.from_now}
+  end
 
   def require_confirmation
     user = User.find_by(email: params[:auth][:email])
--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
+import { Form, Text } from 'informed';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Remember! This binding is necessary to make `this` work in the callback
+    this.submit = this.submit.bind(this);
+    this.setFormApi = this.setFormApi.bind(this);
+  }
+
+  submit() {
+    const {email, password} = this.formApi.getState().values;
+
+    fetch("http://localhost:3000/users/login", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ auth: { email, password } })
+    })
+    .then(response => console.log("response: ", response.json()))
+  }
+
+  getCookie = (name) => {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+
+  getImpulses = () => {
+    const jwt = this.getCookie("jwt");
+    fetch("http://localhost:3000/users/3/impulses", {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + jwt,
+      }
+    })
+  }
+
+  setFormApi(formApi) {
+    this.formApi = formApi;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Login</h1>
+          <Form id="login-form" getApi={this.setFormApi}>
+            <label htmlFor="login-email">Email:</label>
+            <Text field="email" id="login-email" />
+            <label htmlFor="login-password">Password:</label>
+            <Text type="password" field="password" id="login-password" />
+            <button type="submit" onClick={this.submit}>Submit</button>
+          </Form>
+          
+          <button type="submit" onClick={this.getImpulses}>Get Impulses</button>
         </header>
       </div>
     );
