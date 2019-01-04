cf83df2e24bae5bad5b9c5e8572b6e4bb6411109
Shane <shane.boyar@gmail.com>
2019-01-04 15:22:56 -0500

    Require email verification for login

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,13 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
+  before_action :require_confirmation, only: [:create]
+
+  private
+
+  def require_confirmation
+    user = User.find_by(email: params[:auth][:email])
+    if !user.confirmed_at?
+      render json: {error: 'Email not verified' }, status: :unauthorized
+    end
+  end
 end
